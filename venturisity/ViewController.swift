f0f4a270911ba1027ee76e309481d17084f6f898
Saransh Mittal <saranshmanu@yahoo.co.in>
2018-10-02 03:18:23 +0530

    Initial Commit

---
+++ venturisity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  venturisity
+//
+//  Created by Saransh Mittal on 02/10/18.
+//  Copyright © 2018 Saransh Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
